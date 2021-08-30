38d2d3329b28173a51bcf3ec0998d948b07859a9
chenf <chenfu@dhcc.com.cn>
2021-08-30 22:48:59 +0800

    添加客户端异常处理

--- main.py
+++ main.py
@@ -7,17 +7,21 @@ import host_service
 def publish():
     telemetry = host_service.host_info()
 
-    client = TBDeviceMqttClient("192.168.1.14", "5P9m16nkUwW5ekN6K9HA")
+    client = TBDeviceMqttClient("2455r6l881.qicp.vip", "trNsuee16OKl9MVkxCYD", 47793)
     # Connect to ThingsBoard
-    client.connect()
-    # Sending telemetry without checking the delivery status
-    client.send_telemetry(telemetry) 
-    # Sending telemetry and checking the delivery status (QoS = 1 by default)
-    result = client.send_telemetry(telemetry)
-    # get is a blocking call that awaits delivery status  
-    success = result.get() == TBPublishInfo.TB_ERR_SUCCESS
-    # Disconnect from ThingsBoard
-    client.disconnect()
+    try:
+        client.connect()
+        # Sending telemetry without checking the delivery status
+        client.send_telemetry(telemetry) 
+        # Sending telemetry and checking the delivery status (QoS = 1 by default)
+        result = client.send_telemetry(telemetry)
+        # get is a blocking call that awaits delivery status  
+        success = result.get() == TBPublishInfo.TB_ERR_SUCCESS
+    except BaseException:
+        print('连接失败')
+    finally:
+        # Disconnect from ThingsBoard
+        client.disconnect()
 
 
 # 单位：秒
